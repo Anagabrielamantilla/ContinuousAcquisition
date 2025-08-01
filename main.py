724999111b159e55b3261290d4b43f7d6bf60605
Ana Gabriela Mantilla <ana.mantilla@correo.uis.edu.co>
2025-08-01 16:06:53 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,147 @@
+
+import time
+import wandb
+import torch
+import numpy as np
+from utils import *
+from tqdm import trange
+from NetworkPaul import *
+import torch.optim as optim
+from matplotlib import gridspec
+import matplotlib.pyplot as plt
+
+wandb.login(key="d4d6be3e67d35616aec5ede205c72296825c4db1")
+wandb.init(project="ANA_CONTINOUS_ACQUISITION", name="E1")
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+# Load S
+S = np.load("Data/seismic_diffusion_11.npy")
+S = torch.from_numpy(S)
+S = S-S.min()
+S = S/S.max()
+S = S*2 - 1
+S = S.to(device)
+
+print('Maximum value of S', S.max())
+print('Minimum value of S', S.min())
+
+T1, M1 = uniform_sampling(S, 50)
+T1 = T1.to(device)
+M1 = M1.to(device)
+
+batch_size = 4
+dataset = dataset = SeismicDataset(T1, S, M1)
+dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+
+network = AttU_Net(img_ch=10,output_ch=10).to(device)
+
+binary_layer = MaskLayer((1024,128)).to(device)
+reg =  SubsamplingRate(parameter=1, compression=0.9)  
+
+optimizer = optim.Adam(list(network.parameters()) + list(binary_layer.parameters()), lr=1e-3)
+criterion = nn.MSELoss()
+
+start = time.time()
+
+epochs = 100
+
+list_loss1, list_loss2, list_loss3, list_loss_total = [], [], [], []
+
+for epoch in trange(epochs, desc="Entrenamiento"):
+    network.train()
+    binary_layer.train()
+    
+    for batch_T1, batch_S, batch_M1 in dataloader:
+        batch_T1 = batch_T1.to(device)
+        batch_S = batch_S.to(device)
+        batch_M1 = batch_M1.to(device)
+
+        optimizer.zero_grad()
+        
+        M2 = binary_layer(batch_T1)
+        M2 = M2*(1-batch_M1)
+        T2 = M2*batch_S
+        
+        first_term = M2*network(batch_T1)
+        second_term = T2
+
+        loss1 = criterion(first_term, second_term)
+        
+        distance_m1, distance_m2 = binary_distance(batch_M1, M2)
+        loss2 = 1e-1*criterion(distance_m1/128, distance_m2/128)
+        
+        #loss3 = reg(M2)
+        #loss3 = torch.sum((1-M1[0,0,0,:])*M2[0,0,0,:])
+        loss3 = torch.mean(M2*batch_M1)
+
+        loss = loss1 - loss2 + loss3
+
+        list_loss1.append(loss1.item())
+        list_loss2.append(loss2.item())
+        list_loss3.append(loss3.item())
+        list_loss_total.append(loss.item())
+
+        loss.backward()
+        optimizer.step()
+    
+    #loss_mean = list_loss1.mean()
+    # Logging
+    wandb.log({
+         "loss": loss1.mean().item(),
+         "loss1": loss1.item(),
+         "loss2": loss2.item()
+        }, step=epoch)
+
+        # Visualización
+    if (epoch + 1) % 1 == 0:
+        with torch.no_grad():
+            fig, ax = plt.subplots(3, 3, figsize=(12, 10))
+
+            def imshow_with_colorbar(ax, data, title):
+                im = ax.imshow(data, aspect='auto', cmap='gray')
+                ax.set_title(title)
+                cbar = plt.colorbar(im, ax=ax, fraction=0.046, pad=0.04)
+                cbar.ax.tick_params(labelsize=8)
+
+            # Usar primer elemento del batch
+            b0 = 0
+            imshow_with_colorbar(ax[0, 0], batch_S[b0, 0].cpu().numpy(), 'S')
+            imshow_with_colorbar(ax[0, 1], batch_M1[b0, 0, :25, :25].cpu().numpy(), 'M1')
+            imshow_with_colorbar(ax[0, 2], batch_T1[b0, 0].cpu().numpy(), 'T1')
+            imshow_with_colorbar(ax[1, 0], first_term[b0, 0].detach().cpu().numpy(), 'M2*G(T1)')
+            imshow_with_colorbar(ax[1, 1], second_term[b0, 0].detach().cpu().numpy(), 'M2*(I-M1)S')
+            imshow_with_colorbar(ax[2, 0], M2[b0, 0, :25, :25].detach().cpu().numpy(), 'M2')
+            #imshow_with_colorbar(ax[2, 0], M2[b0, 0].detach().cpu().numpy(), 'M2')
+            
+            
+            # Distancia entre máscaras
+            ax[1, 2].plot(distance_m1.detach().cpu(), label='Distance M1', marker='o', color='blue')
+            ax[1, 2].plot(distance_m2.detach().cpu(), label='Distance M2', marker='x', color='green')
+            #ax[1, 2].plot(abs(distance_m1.detach().cpu() - distance_m2.detach().cpu()), color='purple', label='|Dist M1 - M2|')
+            ax[1, 2].set_title('B(M1,r1) - B(M2,r2)')
+            ax[1, 2].legend()
+
+            # Gráfico de pérdidas
+            ax[2, 1].plot(list_loss1, marker='o', color='orange', label='Loss1')
+            ax[2, 1].plot(list_loss2, marker='x', color='cyan', label='Loss2')
+            ax[2, 1].plot(list_loss3, marker='x', color='blue', label='Loss3')
+            ax[2, 1].plot(list_loss_total, linestyle='-', color='red', label='Loss Total')
+            ax[2, 1].set_title('Loss')
+            ax[2, 1].legend()
+            
+            ax[2, 2].axis('off')  # espacio libre
+            
+            plt.tight_layout()
+            wandb.log({"visualización_epoch": wandb.Image(fig)}, step=epoch)
+            plt.close(fig)
+
+end = time.time()
+print('Total time of computing [s]', end-start)
+
+'''
+fig, ax = plt.subplots(1, 3, figsize=(12, 10))
+ax[0].imshow(1-batch_M1[0, 0, :50, :50].detach().cpu().numpy(),aspect='auto', cmap='gray')
+ax[1].imshow(M2[0, 0, :50, :50].detach().cpu().numpy(),aspect='auto', cmap='gray')
+ax[2].imshow(batch_M1[0, 0, :50, :50].detach().cpu().numpy() - M2[0, 0, :50, :50].detach().cpu().numpy(),aspect='auto', cmap='gray')
+'''
---
+++ utils.py
@@ -0,0 +1,117 @@
+import torch
+import torch.fft
+import torch.nn as nn
+from typing import List, Optional
+from torch.autograd import Function
+from torch.utils.data import Dataset, DataLoader
+
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+def uniform_sampling(data, num):
+    indices = torch.linspace(0, data.shape[3] - 1, steps=num).long()
+    mask = torch.ones_like(data)  
+    mask[..., indices] = 0 
+    return data * mask, mask
+    
+def binary_distance(M1, M2):
+    receivers_coordinates = torch.arange(0,128).to(device)
+    fila_M1 = M1[0, 0, 0, :]
+    fila_M2 = M2[0, 0, 0, :] 
+    
+    coord_M1 = fila_M1*receivers_coordinates
+    coord_M2 = fila_M2*receivers_coordinates
+      
+    return coord_M1, coord_M2
+
+class SeismicDataset(torch.utils.data.Dataset):
+    def __init__(self, T1, S, M1):
+        self.T1 = T1
+        self.S = S
+        self.M1 = M1
+
+    def __len__(self):
+        return self.T1.shape[0]
+
+    def __getitem__(self, idx):
+        return self.T1[idx], self.S[idx], self.M1[idx]
+
+# Paso 1 : definir la clase de binarización 
+class BinaryQuantize(Function):
+    @staticmethod
+    def forward(ctx, input):
+        out = torch.sign(input) 
+        out = (out+1)/2
+        return out
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        grad_input = grad_output.clone()
+        return grad_input
+
+
+class BinaryQuantize_a(Function):
+    @staticmethod
+    def forward(ctx, input):
+        ctx.save_for_backward(input)
+        out = torch.sign(input)
+        out = (out+1)/2
+        return out
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        input = ctx.saved_tensors[0]
+        grad_input = (2 - torch.abs(2*input))
+        grad_input = grad_input.clamp(min=0) * grad_output.clone()
+        return grad_input
+        
+        
+# Paso 2:  Definir clase subsampling y forward de lo que se quiere hacer 
+# Esta clase recibe datos de [M,N] es decir [128,128]
+class MaskLayer(nn.Module):
+    def __init__(self, im_size: tuple):
+        super(MaskLayer, self).__init__()
+
+        H, W = im_size
+        self.H = H
+        self.W = W
+        
+        mask = torch.normal(0, 1, size=(1, W)) #trazas
+        mask = mask / torch.sqrt(torch.tensor(W).float())
+        self.mask = nn.Parameter(mask, requires_grad=True)
+
+    def forward(self, inputs):
+        
+        mask = self.get_mask()
+        x = mask*inputs
+        primera_fila = mask[0,0,0,:]
+        
+        # Contar los ceros
+        num_ceros = torch.sum(primera_fila == 0).item()
+        
+        print(f"Columnas removidas: {num_ceros}")
+        
+        return mask
+
+    def get_mask(self):
+        mask = self.mask
+        mask = torch.tile(mask, (self.H, 1))
+        mask = mask.unsqueeze(0).unsqueeze(0)
+        mask = BinaryQuantize.apply(mask)
+        
+        return mask
+        
+        
+# Paso 3: Clase de regularización 
+
+class SubsamplingRate(torch.nn.Module):
+    def __init__(self, parameter, compression):
+        super(SubsamplingRate, self).__init__()
+        self.parameter = parameter
+        self.compression = compression
+
+    def forward(self, mask):
+        regularization = torch.square((torch.sum(mask) / mask.numel()) - self.compression)
+        regularization = self.parameter * regularization
+
+        return regularization
